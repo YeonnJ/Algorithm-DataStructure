35a7f5e07f24f72197d6f4c7506e3310778a8720
YeonJae <duswo9595@naver.com>
2022-03-13 19:06:38 +0900

    We love krii

---
+++ 02.BaekJoon/입출력과사칙연산/We_love_krii.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  console.log("강한친구 대한육군\n강한친구 대한육군");
+});
