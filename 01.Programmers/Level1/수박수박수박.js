e257badb23582fb5983bc8b4139598520c1b1fa0
YeonJae <duswo9595@naver.com>
2022-03-14 11:07:32 +0900

    수박수박수박

---
+++ 01.Programmers/Level1/수박수박수박.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = "";
+
+  for (let i = 0; i < n; i++) {
+    if (i % 2 === 0) {
+      answer += "수";
+    } else if (i % 2 === 1) {
+      answer += "박";
+    }
+  }
+  return answer;
+}
