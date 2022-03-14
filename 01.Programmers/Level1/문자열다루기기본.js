07e41a99d8313d8e048b519685b83d473c916e52
YeonJae <duswo9595@naver.com>
2022-03-14 16:10:11 +0900

    문자열다루기 기본

---
+++ 01.Programmers/Level1/문자열다루기기본.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  if (!(s.length == 4 || s.length == 6)) {
+    return false;
+  } else {
+    let arr = s.split("");
+    for (let i = 0; i < arr.length; i++) {
+      if (isNaN(arr[i])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
