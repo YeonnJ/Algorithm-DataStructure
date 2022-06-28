3e057f8a9f778da13d9d9e7c8aa9d1b91372e27f
YeonJae <duswo9595@naver.com>
2022-06-28 22:33:13 +0900

    최대공약수 최소공배수

---
+++ 01.Programmers/Level1/최대공약수최소공배수.js
@@ -0,0 +1,33 @@
+function solution(n, m) {
+  let answer = [];
+
+  if (n > m) {
+    for (i = 2; i <= n; i++) {
+      if (n % i === 0 && m % i === 0) answer[0] = i;
+    }
+
+    if (answer[0]) {
+      answer[1] = (n * m) / answer[0];
+    }
+
+    if (!answer[0]) {
+      answer[0] = 1;
+      answer[1] = n * m;
+    }
+  } else {
+    for (i = 2; i <= m; i++) {
+      if (n % i === 0 && m % i === 0) answer[0] = i;
+    }
+
+    if (answer[0]) {
+      answer[1] = (n * m) / answer[0];
+    }
+
+    if (!answer[0]) {
+      answer[0] = 1;
+      answer[1] = n * m;
+    }
+  }
+
+  return answer;
+}
